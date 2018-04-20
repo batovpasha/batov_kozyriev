1786fd400cc903e0cbc4587993e118cabfc93df5
fluoxe7ine <locbkolocb@gmail.com>
2018-04-20 16:03:10 +0300

    added pathfinding function

--- assignment-2/main.cpp
+++ assignment-2/main.cpp
@@ -31,47 +31,35 @@ class Queue {
   public:
     bool empty(List *tail) { return tail == nullptr ? true : false; }
     void push(List **head, List **tail, int x, int y){
-      // List *temp = new List;
-      // temp->data.x = x;
-      // temp->data.y = y;
-      // int g = abs(X1 - x) + abs(Y1 - y);
-      // int h = abs(X2 - x) + abs(Y2 - y);
-      // temp->data.G = g;
-      // temp->data.H = h;
-      // int f = g + h;
-      // temp->data.F = f;
-      // temp->next = nullptr;
-      // if (!(*head)) {
-      //   temp->prev = nullptr;
-      //   (*head) = temp;
-      //   (*tail) = temp;
-      // } else {
-      //   temp->prev = (*tail);
-      //   temp->next = nullptr;
-      //   (*tail)->next = temp;
-      //   (*tail) = temp;
-      // }
-      List *el = new List;
-       int g = abs(X1 - x) + abs(Y1 - y);
-       int h = abs(X2 - x) + abs(Y2 - y);
-       int f = g + h;
-       el->data.G = g;
-       el->data.H = h;
-       el->data.x = x;
-       el->data.y = y;
-       el->data.F = f;
-       el->prev = nullptr;
-       el->next = *tail;
-       (*tail)->prev = el;
-       *tail = el;
+      List *temp = new List;
+      temp->data.x = x;
+      temp->data.y = y;
+      int g = abs(X1 - x) + abs(Y1 - y);
+      int h = abs(X2 - x) + abs(Y2 - y);
+      temp->data.G = g;
+      temp->data.H = h;
+      int f = g + h;
+      temp->data.F = f;
+      temp->next = nullptr;
+      if (!(*head)) {
+        temp->prev = nullptr;
+        temp->next = nullptr;
+        (*head) = temp;
+        (*tail) = temp;
+      } else {
+        temp->prev = (*tail);
+        temp->next = nullptr;
+        (*tail)->next = temp;
+        (*tail) = temp;
+      }
     }
 
-    void print(List *head, List *tail){
-      tail = head;
-      while (tail) {
-        cout << tail->data.x << " " << tail->data.y << endl;
+    void print(List *head){
+      List *temp = head;
+      while (temp) {
+        cout << temp->data.x << " " << temp->data.y << endl;
         cout << "--------" << endl;
-        tail = tail->next;
+        temp = temp->next;
       }
     }
 
@@ -85,27 +73,30 @@ class Queue {
     }
   };
 
-  bool include(List **head, List **tail, int x, int y){
-  (*tail) = (*head);
-  while((*tail)){
-    if (((*tail)->data.x == x)&&((*tail)->data.y == y)) return true;
-    (*tail) = (*tail)->next;
-  }
-  return false;
-  }
-
-  List *getMin(List **head){
+  void getMin(List **head){
     List *temp = *head;
     List *min_f = *head;
-    while (temp -> next) {
+    while (temp) {
       if (temp->data.F < min_f->data.F) {
         min_f = temp;
       }
+      temp = temp->next;
     }
-    swap(*head, min_f);
-    swap((*head)->next, min_f->next);
-    swap((*head)->prev, min_f->prev);
-    return *head;
+    swap(min_f->data.x, (*head)->data.x);
+    swap(min_f->data.y, (*head)->data.y);
+    swap(min_f->data.G, (*head)->data.G);
+    swap(min_f->data.H, (*head)->data.H);
+    swap(min_f->data.F, (*head)->data.F);
+
+  }
+
+  bool include(List **head, List **tail, int x, int y){
+  List *temp = (*head);
+  while(temp){
+    if ((temp->data.x == x)&&(temp->data.y == y)) return true;
+    temp = temp->next;
+  }
+  return false;
   }
 
 };
@@ -113,20 +104,22 @@ class Queue {
 void A_star(){
   Queue U; //visited queue
   Queue Q; //unvisited queue
+
   Q.push(&Q_head, &Q_tail, X1, X2);
+  Q.getMin(&Q_head);
   List *current;
 
- // while(!Q.empty(Q_tail)){
-    current = Q.getMin(&Q_head);
+  while(!Q.empty(Q_tail)){
+    Q.getMin(&Q_head);
+    current = Q_head;
     int c_x = current->data.x;
     int c_y = current->data.y;
     if ((c_x == X2) && (c_y == Y2)){
       cout << "Vse zaebis" << endl;
+      return;
     }
-
-    //Q.pop(&Q_head, &Q_tail);
-    // U.push(&U_head, &U_tail, c_x, c_y);
-
+    Q.pop(&Q_head, &Q_tail);
+    U.push(&U_head, &U_tail, c_x, c_y);
     if (((U.include(&U_head, &U_tail, c_x-1, c_y) == false)||(gValue(c_x-1,c_y) < current->data.G))&&(U.include(&U_head, &U_tail, c_x-1,c_y) == false)){
       if (data[c_x-1][c_y] != 'X') Q.push(&Q_head, &Q_tail, c_x-1, c_y);
     }
@@ -139,9 +132,8 @@ void A_star(){
     if (((U.include(&U_head, &U_tail, c_x, c_y-1) == false)||(gValue(c_x,c_y-1) < current->data.G))&&(U.include(&U_head, &U_tail, c_x,c_y-1) == false)){
       if (data[c_x][c_y-1] != 'X') Q.push(&Q_head, &Q_tail, c_x, c_y-1);
     }
-    Q.print(Q_head, Q_tail);
+ }
 
-// }
 }
 
 
